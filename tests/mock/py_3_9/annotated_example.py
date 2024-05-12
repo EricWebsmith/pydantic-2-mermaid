dde9ee736f0ba4a3eb10fb9e21133d5288da7c4e
eric <eric>
2024-05-12 11:30:37 +0800

    fix python 3.9

--- tests/mock/py_3_9/annotated_example.py
+++ tests/mock/py_3_9/annotated_example.py
@@ -1,10 +1,13 @@
-from typing import Annotated
+import sys
+from typing import Dict
 
-from pydantic import BaseModel, Field
+if sys.version_info >= (3, 9):
+    from typing import Annotated
 
+    from pydantic import BaseModel, Field
 
-class AnotatedExample(BaseModel):
-    int_prop: Annotated[int, Field(ge=0, le=100)]
-    dict_prop: dict[str, Annotated[float, Field(ge=0.1, le=1.0)]] = Field(
-        description="Axis stiffness factors", default_factory=dict
-    )
+    class AnotatedExample(BaseModel):
+        int_prop: Annotated[int, Field(ge=0, le=100)]
+        dict_prop: Dict[str, Annotated[float, Field(ge=0.1, le=1.0)]] = Field(
+            description="Axis stiffness factors", default_factory=dict
+        )
